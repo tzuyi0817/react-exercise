65a89e05525ae6426a27c6f55529e1cf91a55cca
Tzuyi Yang <a0988426059@gmail.com>
2020-05-01 14:50:17 +0800

    Add remove todo

--- src/components/TodoExample/TodoList.js
+++ src/components/TodoExample/TodoList.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { toggleTodo } from '../../actions'
+import { deleteTodo } from '../../actions'
 
-const Todo = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    className="mt-2"
-    style={{
-      textDecoration: completed ? 'line-through' : 'none',
-    }}
-  >
-    {text}
-  </li>
+const deleteBtn = {
+  float: 'right'
+}
+
+const Todo = ({ onClick, completed, text, onDelete }) => (
+  <div className="todo-list">
+    <button type="click" className="btn btn-sm btn-danger" style={deleteBtn} onClick={onDelete}>x</button>
+    <li
+      onClick={onClick}
+      className="mt-2"
+      style={{
+        textDecoration: completed ? 'line-through' : 'none',
+        border: 'none'
+      }}
+    >
+      {text}
+    </li>
+  </div>
 )
 
-const TodoList = ({ todos, toggleTodo }) => (
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => (
   <ul className="mt-3" style={{ flexDirection: 'column', width: '20vw' }}>
     {todos.map(todo => (
-      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} onDelete={() => deleteTodo(todo.id)} />
     ))}
   </ul>
 )
@@ -27,7 +36,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleTodo(id))
+  toggleTodo: id => dispatch(toggleTodo(id)),
+  deleteTodo: id => dispatch(deleteTodo(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -15,6 +15,11 @@ const todos = (state = [], action) => {
         todo => todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
       )
 
+    case 'DELETE_TODO':
+      return state.filter(
+        todo => todo.id !== action.id
+      )
+
     default: return state
   }
 }
